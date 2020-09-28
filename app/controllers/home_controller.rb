126ee696747fd4d42c7d8a443b0f6ffcfa434be1
Lincoln <lincolnwilson16@gmail.com>
2020-09-28 18:04:59 +0000

    Basic UI and API Integration

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,43 @@
 class HomeController < ApplicationController
   def index
+  end  
+  def search
+    movies = find_movie(params[:movie].gsub(' ','+')).first.second    
+    unless movies
+      flash[:alert] = 'Movie Not Found'
+      return render action: :index
+    end
+    if movies == "False"
+      flash[:alert] = 'Movie Not Found'
+      return render action: :index
+    else
+      @movie = movies
+    end
   end
-  
-  def standings
-    standings = find_standings(params[:team])
+  def createAccount
+	@username = User.new(user_params)
+	@username.save
+	redirect_to @article
   end
-  
+  def login
+	@user = User.find(user_params)
+	if @user.username == :username
+		login()
+	else
+		alert("Incorrect Login")
+	end
+  end
   private
+  require 'rubygems'
+  require 'httparty'
+  include HTTParty
+  base_uri "omdbapi.com"
+  def find_movie(name)
+	self.class.get("/?apikey=APIKEYHERE&s=#{name}")
+  end
+  def user_params
+	params.require(:user).permit(:username,:password)
+  end
 end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get 'home/index'
   root 'home#index'
   
-  get '/teams' => 'home#teams'
+  get '/search' => 'home#search'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
